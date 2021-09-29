c6f67ef7687ae0e47a521c10156ee6d401437be5
SimiliSerpent <benjamin.vacus@polytechnique.edu>
2021-09-29 18:58:26 +0200

    Added create_table.sql - sql script to initialize the DB.

---
+++ create_tables.sql
@@ -0,0 +1,51 @@
+CREATE SCHEMA annotation_seq;
+SET SCHEMA 'annotation_seq';
+
+
+CREATE TABLE users(
+  email VARCHAR,
+  pw VARCHAR,
+  first_name VARCHAR,
+  last_name VARCHAR,
+  phone VARCHAR,
+  role VARCHAR,
+  status VARCHAR,
+  date_of_validation TIMESTAMP,
+  CHECK (role IN ('lector', 'annotator', 'validator', 'administrator')),
+  CHECK (status IN ('waiting', 'validated')),
+  PRIMARY KEY (email)
+);
+
+CREATE TABLE genome(
+  genome_id VARCHAR,
+  PRIMARY KEY (genome_id)
+);
+
+CREATE TABLE gene(
+  gene_id VARCHAR,
+  genome_id VARCHAR,
+  size INTEGER,
+  chromosome VARCHAR,
+  localization VARCHAR,
+  type VARCHAR,
+  CHECK (type IN ('pep', 'cds')),
+  PRIMARY KEY (gene_id),
+  FOREIGN KEY (genome_id) REFERENCES genome(genome_id)
+);
+
+CREATE TABLE annotations(
+  genome_id VARCHAR,
+  gene_id VARCHAR,
+  gene_biotype VARCHAR,
+  transcript_biotype VARCHAR,
+  gene_symbol VARCHAR,
+  description VARCHAR,
+  annotator VARCHAR,
+  status VARCHAR,
+  date_of_validation TIMESTAMP,
+  CHECK (status IN ('waiting', 'validated')),
+  PRIMARY KEY (annotator, genome_id, gene_id),
+  FOREIGN KEY (annotator) REFERENCES users(email),
+  FOREIGN KEY (genome_id) REFERENCES genome(genome_id),
+  FOREIGN KEY (gene_id) REFERENCES gene(gene_id)
+);
