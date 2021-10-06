7a691561b27c400d1cd1adfbcde9f129e1e647a3
Soundous <soudous.bb@gmail.com>
2021-10-06 11:37:02 +0200

    Improved login page

--- site_1.php
+++ site_1.php
@@ -8,28 +8,27 @@
 
   <body>
     <div id="menu">
-      Le menu
+      Welcome to our brand new website login page!<br>
+      Spoiler: nothing is working yet.
     </div>
-    Hello world! <br>
 
     <div id="element1">
-    <form action="TRUC TRUC.php" method="post">
-      <table>
-        <tr>
-          <td> Login :
-          </td>
-          <td> <input type="text" name="name"> </td>
-        </tr>
-        <tr>
-          <td> Password : </td>
-          <td> <input type="password" name="pass"> </td>
+      <form action="TRUC TRUC.php" method="post">
+        <table class="center">
+          <tr>
+            <td> Login : </td>
+            <td> <input type="text" name="name"> </td>
+          </tr>
+          <tr>
+            <td> Password : </td>
+            <td> <input type="password" name="pass"> </td>
+          </tr>
+          <tr>
+            <td colspan=2> <input type ="submit" value="Log in"> </td>
+          </tr>
+        </table>
 
-        </tr>
-        <tr> <input type ="submit" value="Log in">
-        </tr>
-      </table>
-
-    </form>
+      </form>
     </div>
   </body>
 
