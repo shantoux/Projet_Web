ab458d2e695fbae9f98c8c1aa66c68f12f23b3e2
Soundous <soudous.bb@gmail.com>
2021-10-06 11:22:55 +0200

    site_version_0

---
+++ site_1.php
@@ -0,0 +1,36 @@
+<html>
+
+  <head>
+    <meta charset="UTF-8">
+    <title>Website_title</title>
+    <link rel="stylesheet" type="text/css" href="./pw_style.css">
+  </head>
+
+  <body>
+    <div id="menu">
+      Le menu
+    </div>
+    Hello world! <br>
+
+    <div id="element1">
+    <form action="TRUC TRUC.php" method="post">
+      <table>
+        <tr>
+          <td> Login :
+          </td>
+          <td> <input type="text" name="name"> </td>
+        </tr>
+        <tr>
+          <td> Password : </td>
+          <td> <input type="password" name="pass"> </td>
+
+        </tr>
+        <tr> <input type ="submit" value="Log in">
+        </tr>
+      </table>
+
+    </form>
+    </div>
+  </body>
+
+</html>
