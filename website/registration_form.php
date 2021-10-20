71f0ccce6eea31ff0cc9c714f7625c2f6314f174
Soundous <soudous.bb@gmail.com>
2021-10-21 01:14:45 +0200

    changes

--- website/registration_form.php
+++ website/registration_form.php
@@ -1,39 +1,46 @@
 <html>
-<head> <meta charset="UTF-8">
-<title>Registration form</title>
-<link rel="stylesheet" type="text/css" href="./pw_style.css">
-</head>
+
+  <head> <meta charset="UTF-8">
+    <title>Registration form</title>
+    <link rel="stylesheet" type="text/css" href="./pw_style.css">
+  </head>
 
 <body>
   <div id="menu">
     Please register so you can join in on the fun !
   </div>
-
+  
+<div id="element1">
 <form>
   <table class="center">
 
     <tr>
-      <td> Email address : </td>
+      <td> <div>
+        <label>Email address : </label></td>
       <td> <input type="text" name="adress"> </td>
     </tr>
 
     <tr>
-      <td> Last name : </td>
+      <td> <div>
+        <label>Last name : </label></td>
       <td> <input type="text" name="lastname"> </td>
     </tr>
 
     <tr>
-      <td> First name : </td>
+      <td> <div>
+        <label>First name : </label></td>
       <td> <input type="text" name="firstname"> </td>
     </tr>
 
     <tr>
-      <td> Phone number : </td>
+      <td> <div>
+        <label>Phone number : </label></td>
       <td> <input type="text" name="phone"> </td>
     </tr>
 
     <tr>
-      <td> Role : </td>
+      <td> <div>
+        <label>Role : </label></td>
       <td><select name="role">
         <option value="Reader"> Reader </option>
         <option value="validator"> Validator </option>
@@ -41,8 +48,12 @@
       </select>
     </td>
     </tr>
-
+    <tr>
+      <td colspan=2> <input type ="submit" value="Log in" name = "submit"> </td>
+    </tr>
 
   </table>
 </form>
+</div>
+</body>
 </html>
