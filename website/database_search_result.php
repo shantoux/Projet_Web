b9ddfa1c630930c4595e577b45a562eca2148b42
Soundous <soudous.bb@gmail.com>
2021-10-21 14:15:01 +0200

    updated some pages

---
+++ website/database_search_result.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head> <meta charset="UTF-8">
+  <title>Database search result</title>
+  <link rel="stylesheet" type="text/css" href="pw_style.css" />
+</head>
+
+<body>
+
+  <div id="element1">
+    Search results
+  </div>
+
+  Genome's type : [insert type protein or nucleotides]<br>
+  <h2>Results:</h2>
+
+<div class="table-wrapper">
+  <table class="ft-table">
+
+    <thead>
+        <tr>
+            <th>Genome</th>
+            <th>Species</th>
+            <th>Sequences</th>
+        </tr>
+        </thead>
+
+  </table>
+</div>
+</body>
+</html>
