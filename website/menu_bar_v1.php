3218d58b7359cad96d2b23edb9101244580577e9
SimiliSerpent <benjamin.vacus@polytechnique.edu>
2021-10-21 11:47:21 +0200

    Try on css bug

--- website/menu_bar_v1.php
+++ website/menu_bar_v1.php
@@ -3,7 +3,7 @@
 <!DOCTYPE html>
 <html>
 
-  <head> <meta charset="UTF-8">
+  <head> <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Navigation </title>
     <link rel="stylesheet"  href="pw_style.css">
   </head>
