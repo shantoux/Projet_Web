ebc0edef2d07f700b71b60539bf896205a15e4a5
SimiliSerpent <benjamin.vacus@polytechnique.edu>
2021-12-02 11:09:41 +0100

    Modified genome visualization

--- website/genome_info.php
+++ website/genome_info.php
@@ -38,11 +38,32 @@
       Genome information
     </div>
 
+    <!-- store genome -->
+    <?php
+      $sequences = array("CGATCGATGAGCAGCTTTGCATGCAGAAACGATCGGCGCGCTAGTACGCCCGGCTGCATGCAGAAACGATCGGCGCGCTAGTACGATCGTCAGGATCACTACGCAGCACTAGC",
+                          "ATGCGTACGATCGTGACATCTGATCGTCTCTAGCTAGCATCTGGCATCG",
+                          "GCTCGGGATACGCTCAGCTGGAGCCTGGCTATCATGCGAGCTAGGC",
+                          "ATGCAGTGAGCGCGATCGAGACGCTGATGATCGTAGACGTCGA",
+                          "CGATCGATGAGCAGCTTCCCGGCTGCATGCAGAAACGATCGGCGCGCTAGTACGATCGTCAGGATCACTACGCAGCACTAGC",
+                          "ATGCGATGCAATCTGCTAGACAGCTACGC");
+      $genome_fragments = array(array("seq"=>$sequences[0], "type"=>"igene", "id"=>"0"),
+                                array("seq"=>$sequences[1], "type"=>"gene", "id"=>"1", "annotated"=>false, "name"=>"PHO1"),
+                                array("seq"=>$sequences[2], "type"=>"igene", "id"=>"1"),
+                                array("seq"=>$sequences[3], "type"=>"gene", "id"=>"2", "annotated"=>true, "info"=>"That's a very uncommon gene.", "name"=>"PHO2"),
+                                array("seq"=>$sequences[4], "type"=>"igene", "id"=>"2"),
+                                array("seq"=>$sequences[5], "type"=>"gene", "id"=>"3", "annotated"=>true, "info"=>"That's the 6th finger gene.", "name"=>"PHO3"));
+      echo "<br>";
+    ?>
+
     <div class="center">
-      <table class="table_type2">
+      <table class="table_type_gene_inf">
+        <colgroup>
+          <col span="1" style="width: 10%;">
+          <col span="1" style="width: 90%;">
+        </colgroup>
         <thead>
           <tr>
-            <th colspan=3 class="type2">Genome's name : Ecoli</th>
+            <th colspan=2 class="type2">Genome's name : Ecoli</th>
           </tr>
         </thead>
 
@@ -52,34 +73,35 @@
               extract
               <input type="checkbox" id="Select" name="select">
             </td>
-          <td>
-            ATGAAACGCATTAGCACCACCATTACCACCACCATCACCATTACCACAGGTAACGGTGCGGGCTGA
-          </td>
-          <td>
-            Annotated<br>
-            By [annotator]<br>
-            on 17-11-2021
-          </td>
-        </tr>
-
-        <tr>
-          <td>
-            extract
-            <input type="checkbox" id="Select" name="select">
-          </td>
-          <td>ATGAAACGCATTAGCACCACCATTACCACCACCATCACCATTACCACAGGTAACGGTGCGGGCTGA </td>
-          <td>
-            Annotated<br>
-            By [annotator]<br>
-            on 17-11-2021
-          </td>
-        </tr>
-
+            <td>
+              <?php
+                foreach ($genome_fragments as $fragment) {
+                  if ($fragment["type"] == "igene") {
+                    echo '<span style="font-family:Consolas;">';
+                  }
+                  else {
+                    if ($fragment["annotated"]) {
+                      $color = "blue";
+                      $info = ' title="' . $fragment["name"] . "\n" . $fragment["info"] . "\nClick to see annotation";
+                      echo '<span style="font-family:Consolas;color:' . $color . ';"' . $info . ';">';
+                    }
+                    else {
+                      $color = "red";
+                      $info = ' title="' . "WARNING: Unannotated gene";
+                      echo '<span style="font-family:Consolas;color:' . $color . ';"' . $info . '">';
+                    }
+                  }
+                  print_r($fragment["seq"]);
+                  echo '</span>';
+                  #print_r($fragment[0]);
+                }
+              ?>
+            </td>
         </tbody>
       </table>
     </div>
 
-    Extract: 
+    Extract:
     <a href="path_to_file" download="name_file">
          <button type="button">Download</button>
          </a>
