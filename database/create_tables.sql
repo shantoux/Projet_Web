3501defcde406fd3f65da7cabd13c3e4f21746c6
Soundous_BellaBaci <61917824+Sounbouh@users.noreply.github.com>
2021-12-03 15:20:47 +0100

    Update create_tables.sql

--- database/create_tables.sql
+++ database/create_tables.sql
@@ -30,7 +30,7 @@ CREATE TABLE gene(
   chromosome VARCHAR,
   prot_seq VARCHAR(10000),
   gene_seq VARCHAR(10000),
-  PRIMARY KEY (gene_id),
+  PRIMARY KEY (sequence_id),
   FOREIGN KEY (genome_id) REFERENCES genome(genome_id)
 );
 
