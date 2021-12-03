c1a416794fc07d73795f52a8b1fe32d958e43cfb
Soundous_BellaBaci <61917824+Sounbouh@users.noreply.github.com>
2021-12-03 11:36:39 +0100

    Delete test.php

--- test.php
+++
@@ -1,8 +0,0 @@
-<?php
-  //Selectionner la bonne connexion
-  //--------------------------------
-//phpinfo();
-  $db = pg_connect("host=localhost port=5432 dbname=public username = soundous") or die ("Connection échoué");
-
-
-?>
