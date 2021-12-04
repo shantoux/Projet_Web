01d8d3ede0a1d0636dd963de72a00bd16546470b
SimiliSerpent <benjamin.vacus@polytechnique.edu>
2021-12-04 14:24:29 +0100

    Update reset_database.sh

--- reset_database.sh
+++ reset_database.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/env bash
 
-while read -r
-do [[ $line =~ ^(host) ]] && echo "$line"
+while read -r; do
+  if [[ $REPLY =~ ^(host) ]]; then
+    $host = ${REPLY##* }
+  elif [[ $REPLY =~ ^(user) ]]; then
+    $user = ${REPLY##* }
+  elif [[ $REPLY =~ ^(password) ]]; then
+    $pw = ${REPLY##* }
+  fi
 done < $1
+
+echo $host
+echo $user
+echo $pw
