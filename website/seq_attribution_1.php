642ea2bb0af5aa8ad359204e53571594251d96ec
Soundous <soudous.bb@gmail.com>
2021-12-04 22:26:18 +0100

    --

--- website/seq_attribution_1.php
+++ website/seq_attribution_1.php
@@ -68,7 +68,7 @@
             <td> Ecoli</td>
             <td>
               <select name="annotator">
-                <option value="annotator"> <?php echo ({"$_SESSION['first_name']")};?></option>
+                <option value="annotator"> <?php echo ({"$_SESSION['first_name']"});?></option>
                 <!--<option value="annotator"> </option>-->
               </select>
 
