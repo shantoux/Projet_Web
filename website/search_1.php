78d98ea2b5311d948d88e32d5fbe43e42c0f5057
shantoux <sandra_95170@hotmail.com>
2021-12-05 22:10:46 +0100

    update WTF

--- website/search_1.php
+++ website/search_1.php
@@ -55,9 +55,21 @@
     <div id="element1">
       <form action="database_search_result.php" method = "post">
         <table class="center">
+        <tr>
+            <td> Type of search:<span style="color:red;">*</span></td>
+            <td align="left"> <input type="radio" id="genome" name="search_type" value="genome" required> <label for="genome">Genome</label> </td>
+          <tr>
+            <td></td>
+            <td align="left"> <input type="radio" id="gene_prot" name="search_type" value="gene_prot" required> <label for="gene_prot">Gene / Proteine</label> </td>
+
+          </tr>
           <tr>
             <td> Specie / Name / Strain: </td>
             <td> <input type="text" name="specie"> </td>
+          </tr>
+          <tr>
+            <td> Sequence identifier: </td>
+            <td> <input type="text" name="seq_id"> </td>
           </tr>
           <tr>
             <td> Nucleotides sequence: </td>
@@ -76,16 +88,9 @@
             <td> <textarea cols="29" rows="5" name="description"></textarea> </td>
           </tr>
           <tr>
-            <td> Type of search:<span style="color:red;">*</span></td>
-            <td align="left"> <input type="radio" id="genome" name="search_type" value="genome" required> <label for="genome">Genome</label> </td>
-          <tr>
-            <td></td>
-            <td align="left"> <input type="radio" id="gene_prot" name="search_type" value="gene_prot" required> <label for="gene_prot">Gene / Proteine</label> </td>
-
-          </tr>
-          <tr>
             <td colspan=2> <br> <br> <input type ="submit" value="Search!" name = "submit"> </td>
           </tr>
+
         </table>
         <br> <span class="small_text"> Fields with a <span style="color:red">*</span> are required.</span>
 
