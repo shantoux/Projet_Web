b4ed53d34f4f7dec0f748ca80acf8c0446c8851c
SimiliSerpent <benjamin.vacus@polytechnique.edu>
2021-12-05 10:05:51 +0100

    Wtf this DB

--- website/genome_info.php
+++ website/genome_info.php
@@ -110,7 +110,7 @@
             echo "<td align='left'>";
 
             # retrieve all genes
-            $query = "SELECT sequence_id, start_seq, end_seq, gene_seq FROM annotation_seq.gene WHERE genome_id = '" . $genome_id . "' ORDER BY start_seq LIMIT 10;";
+            $query = "SELECT sequence_id, start_seq, end_seq, gene_seq FROM annotation_seq.gene WHERE genome_id = '" . $genome_id . "' ORDER BY start_seq LIMIT 3;";
             $result = pg_query($db_conn, $query) or die('Query failed with exception: ' . pg_last_error());
             $nucl_ind_count = 0;
             $count = $char_per_line;
