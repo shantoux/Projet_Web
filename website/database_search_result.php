4d098dce6f39a1129c108c59dc928270a9b59e06
SimiliSerpent <benjamin.vacus@polytechnique.edu>
2021-12-05 22:53:11 +0100

    Update database_search_result.php

--- website/database_search_result.php
+++ website/database_search_result.php
@@ -43,6 +43,7 @@
   <!-- Display table of results for the search -->
   <div id="element1">
     <?php
+      ### DISPLAY RESULTS AS GENOMES
       if ($_POST["search_type"] == "genome") {
       echo '<table class = "table_type1">';
 
@@ -114,7 +115,7 @@
         if ($_POST["pep_sequence"] != "") {
           if (!$first_cond) {$query = $query . " AND ";}
           else {$first_cond = false;}
-          $query = $query . "GENE.genome_seq LIKE '%" . $_POST["pep_sequence"] . "%'";
+          $query = $query . "GENE.prot_seq LIKE '%" . $_POST["pep_sequence"] . "%'";
         }
         # check for condition on genes names
         if ($_POST["genes"] != "") {
@@ -150,6 +151,111 @@
 
       echo '</tbody>';
       echo '</table>';
+      }
+
+      ### DISPLAY RESULTS AS GENE/PROT
+      if ($_POST["search_type"] == "gene_prot") {
+        echo '<table class = "table_type1">';
+
+        # display first line
+        echo '<thead>';
+        echo '<tr>';
+        echo '<th>Sequence identifier</th>';
+        echo '<th>Specie</th>';
+        echo '<th>Size of sequence</th>';
+        echo '<th>Annotated</th>';
+        echo '</tr>';
+        echo '</thead>'; #end of first line
+
+        # display results of search
+        echo '<tbody>';
+        $query = "SELECT DISTINCT GENE.seq_id, GENE.genome_id, GENE.gene_seq FROM annotation_seq.gene AS GENE";
+        $conditions = false;
+        # test if one condition has been filled in the search form
+        foreach (array("specie", "nucl_sequence", "seq_id", "pep_sequence") as $token) {
+          if ($_POST[$token] != "") {
+            $conditions = true;
+            break;
+          }
+        }
+        foreach (array("genes", "description") as $token) {
+          if ($_POST[$token] != "") {
+            $conditions = true;
+            $query = $query . ", annotation_seq.annotations AS A";
+            break;
+          }
+        }
+        # add conditions to request
+        if ($conditions) {
+          $first_cond = true;
+          $query = $query . " WHERE ";
+          # joint the tables
+          if ($_POST["genes"] != "" || $_POST["description"] != "") {
+            $first_cond = false;
+            $query = $query . "GENE.genome_id = A.genome_id AND GENE.sequence_id = A.sequence_id";
+          }
+          # check for condition on specie name
+          if ($_POST["specie"] != "") {
+            if (!$first_cond) {$query = $query . " AND ";}
+            else {$first_cond = false;}
+            $query = $query . "GENE.genome_id LIKE '%" . $_POST["specie"] . "%'";
+          }
+          # check for condition on sequence id
+          if ($_POST["seq_id"] != "") {
+            if (!$first_cond) {$query = $query . " AND ";}
+            else {$first_cond = false;}
+            $query = $query . "GENE.sequence_id LIKE '%" . $_POST["seq_id"] . "%'";
+          }
+          # check for condition on nucleotides sequence
+          if ($_POST["nucl_sequence"] != "") {
+            if (!$first_cond) {$query = $query . " AND ";}
+            else {$first_cond = false;}
+            $query = $query . "GENE.gene_seq LIKE '%" . $_POST["nucl_sequence"] . "%'";
+          }
+          # check for condition on peptides sequence
+          if ($_POST["pep_sequence"] != "") {
+            if (!$first_cond) {$query = $query . " AND ";}
+            else {$first_cond = false;}
+            $query = $query . "GENE.prot_seq LIKE '%" . $_POST["pep_sequence"] . "%'";
+          }
+          # check for condition on genes names
+          if ($_POST["genes"] != "") {
+            if (!$first_cond) {$query = $query . " AND ";}
+            else {$first_cond = false;}
+            $query = $query . "A.gene_id LIKE '%" . $_POST["genes"] . "%'";
+          }
+          # check for condition on genes descriptions
+          if ($_POST["description"] != "") {
+            if (!$first_cond) {$query = $query . " AND ";}
+            else {$first_cond = false;}
+            $query = $query . "A.description LIKE '%" . $_POST["description"] . "%'";
+          }
+        }
+        # query is complete!
+        $query = $query . ";";
+        # run query
+        $result = pg_query($db_conn, $query) or die('Query failed with exception: ' . pg_last_error());
+        if(pg_num_rows($result) > 0){
+          for ($res_nb = 0; $res_nb < pg_num_rows($result); $res_nb++) {
+            $s_id = pg_fetch_result($result, $res_nb, 0);
+            $g_id = pg_fetch_result($result, $res_nb, 1);
+            $s_size = strlen(pg_fetch_result($result, $res_nb, 2));
+            echo '<tr><td>';
+            echo "<a href=\"./gene_protein_info.php?id=" . $s_id . "\">$s_id</a></td>";
+            echo "<td><a href=\"./genome_info.php?id=" . $g_id . "\">$g_id</a></td>";
+            echo "<td>$g_size</td><td>";
+            echo "&#10004";
+            echo '</td></tr>';
+          }
+        }
+        else{
+          echo "<div class=\"alert_bad\">
+          <span class=\"closebtn\" onclick=\"this.parentElement.style.display='none';\">&times;</span>
+          No result found in base.</div>";
+        }
+
+        echo '</tbody>';
+        echo '</table>';
       }
     ?>
   </div>
