0c628223d5701af365e47bcc07d8b42d115e0063
SimiliSerpent <benjamin.vacus@polytechnique.edu>
2021-12-05 01:07:54 +0100

    typo

--- website/database_search_result.php
+++ website/database_search_result.php
@@ -131,7 +131,7 @@
           $g_id = pg_fetch_result($result, $res_nb, 0); //récupère le résultat de la 1e colonne (0), $res_nb ieme ligne ($res_nb)
           $g_size = sizeof(pg_fetch_result($result, $res_nb, 1));
           echo '<tr><td>';
-          echo "$g_id</td><td>$g_size"
+          echo "$g_id</td><td>$g_size";
           echo '</td></tr>';
         }
       }
