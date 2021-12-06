9c6bb4121ab52f02e1fd21a552d52c0676bfa70d
shantoux <sandra_95170@hotmail.com>
2021-12-06 16:00:47 +0100

    start

--- website/annotation_1.php
+++ website/annotation_1.php
@@ -42,42 +42,54 @@
       $sequence = "ATGAAACGCATTAGCACCACCATTACCACCACCATCACCATTACCACAGGTAACGGTGCGGGCTGA";
     ?>
 
-    <div class = "table_type1">
-      <table>
-        <colgroup>
-          <col style="width: 20%">
-          <col style="width: 75%">
-          <col style="width: auto">
-        </colgroup>
-        <thead>
-            <tr>
-                <th>Assignation date</th>
-                <th>Sequences</th>
-                <th></th>
-            </tr>
-            </thead>
+<div class="table_type1">
+    <table>
+      <colgroup>
+        <col style="width: 13%">
+        <col style="width: 25%">
+        <col style="width: 10%">
+        <col style="width: 15%">
+        <col style="width: 18%">
+        <col style="width: auto">
+      </colgroup>
+      <thead>
+        <tr>
+          <th>Génomes</th>
+          <th>Sequences</th>
+          <th>Action</th>
+        </tr>
+      </thead>
 
-            <tbody>
-            <tr>
-                <td> <?php echo $assignation_date; ?> </td>
-                <td>
-                  <?php
-                    echo substr($sequence, 0, 50);
-                    if (strlen($sequence) > 50) {
-                      echo "...";
-                    }
-                  ?>
-                </td>
-                <td>
-                  <form action="./annotation_2.php">
-                    <button type="submit">Annotate</button>
-                  </form>
-                </td>
-            </tr>
+      <tbody>
+        <?php
+        include_once 'libphp/dbutils.php';
+        connect_db();
+        $query = "SELECT a.genome_id, a.sequence_id
+        FROM annotation_seq.annotations a
+        WHERE a.annotator ='" .$_SESSION['user']. "' and a.status is null;";
+        $result = pg_query($db_conn, $query);
+        if ($result != false) {
+          while ($rows = pg_fetch_array($result)) {
+            echo "<tr>";
+            echo "<td>" . $rows["genome_id"] . "</td>";
+            echo "<td><a href=\"./sequence_annotation.php?seq=" . $rows["sequence_id"] . "\">" . $rows["sequence_id"] . "</a></td>";
+            # Review annotation
+            echo '<td> <form action="annotation_1.php?seq=' .$rows["sequence_id"]. '" method = "post">';
+            echo '<td> <input type="button" class="button_active" onclick="location.href=\'annotation_1.php?seq=' .$rows["sequence_id"]. '\';"/></td>';
+            echo "</tr>";
+          }
+        } else {
+          echo "
+        <tr>
+        <td colspan='3'>Something went wrong with the query</td>
+        </tr>
+    ";
+        }
+        ?>
+      <tbody>
+    </table>
 
-            <tbody>
-      </table>
-    </div>
+  </div>
 
   </body>
 </html>
