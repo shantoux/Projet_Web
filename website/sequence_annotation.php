d2f7de5f459365fa63b2de99e323a172966ca64a
shantoux <sandra_95170@hotmail.com>
2021-12-07 11:27:28 +0100

    essai shanti

--- website/sequence_annotation.php
+++ website/sequence_annotation.php
@@ -69,7 +69,7 @@
 
       $query2 = "SELECT g.gene_seq, g.prot_seq, g.start_seq, g.end_seq, g.chromosome
       FROM database_projet.gene g
-      WHERE g.sequence_id = " . $sequence_id . ";";
+      WHERE g.sequence_id = '" . $sequence_id . "';";
       $result2 = pg_query($db_conn, $query2) or die('Query failed with exception: ' . pg_last_error());
       $nt = pg_fetch_result($result2, 0, 0);
       $prot = pg_fetch_result($result2, 0, 1);
@@ -79,7 +79,7 @@
       ?>
 
     <div class="center">
-      <form action="./sequence_annotation.php?gid=' . $genome_id . '&sid=' . $sequence_id .  '" method = "post">
+      <form action="<?php echo $_SERVER['PHP_SELF'];?>" method = "post">
         <table class="table_type3">
           <tr colspan=2>
             <td>
