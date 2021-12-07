3a7b6a0a59069d0a76015d81b609db7a7eae4590
SimiliSerpent <benjamin.vacus@polytechnique.edu>
2021-12-07 21:29:26 +0100

    Update download_fasta.php

--- website/download_fasta.php
+++ website/download_fasta.php
@@ -8,13 +8,59 @@ if ($_POST["extracted"]){
   // build text to write in file
   $file_text = '';
 
+  $first_seq = true;
+
   // run through selected sequences
   foreach ($_POST['extracted_seq'] as $seq_id) {
-    $query_seq = "SELECT genome_id, start_seq,end_seq, chromosome, gene_seq FROM database_projet.annotations WHERE genome_id = '" . $g_id . "' AND sequence_id = '" . $s_id . "' AND status != 'rejected';";
+    if (!$first_seq) {
+      $file_text .= '\n;\n';
+    }
+    $query_gene = "SELECT genome_id, start_seq, end_seq, chromosome, gene_seq FROM database_projet.gene WHERE  sequence_id = '" . $seq_id . "';";
+    $result_gene = pg_query($db_conn, $query_gene) or die('Query failed with exception: ' . pg_last_error());
+    $file_text .= '>' . $seq_id . ' cds chromosome:';
+    // add chromosome
+    $file_text .= pg_fetch_result($result_gene, 0, 3);
+    // add start and end position of sequence
+    $file_text .= ':Chromosome:' . pg_fetch_result($result_gene, 0, 1) . ':' . pg_fetch_result($result_gene, 0, 2) . ':';
+
+    // store sequence
+    $seq = pg_fetch_result($result_gene, 0, 4);
+
+    // check if annotation, if yes add it
+    $query_annot = "SELECT gene_id, gene_biotype, transcript_biotype, gene_symbol, description
+    FROM database_projet.annotations
+    WHERE sequence_id = '" . $seq_id . "' AND status != 'rejected';";
     $result_annot = pg_query($db_conn, $query_annot) or die('Query failed with exception: ' . pg_last_error());
-    $new_conv_member['topic_name'] = $_POST['topic_name'];
-    $new_conv_member['user_email'] = $user_email;
-    $result_insert_2 = pg_insert($db_conn, 'database_projet.correspondents', $new_conv_member);
+    if(pg_num_rows($result_annot) > 0){
+      //add gene_id
+      if (pg_fetch_result($result_gene, 0, 0) != "") {
+        $file_text .= ' gene:' . pg_fetch_result($result_gene, 0, 0);
+      }
+      //add gene_biotype
+      if (pg_fetch_result($result_gene, 0, 1) != "") {
+        $file_text .= ' gene_biotype:' . pg_fetch_result($result_gene, 0, 1);
+      }
+      //add transcript_biotype
+      if (pg_fetch_result($result_gene, 0, 2) != "") {
+        $file_text .= ' transcript_biotype:' . pg_fetch_result($result_gene, 0, 2);
+      }
+      //add gene_symbol
+      if (pg_fetch_result($result_gene, 0, 3) != "") {
+        $file_text .= ' gene_symbol:' . pg_fetch_result($result_gene, 0, 3);
+      }
+      //add description
+      if (pg_fetch_result($result_gene, 0, 4) != "") {
+        $file_text .= ' description:' . pg_fetch_result($result_gene, 0, 4);
+      }
+    }
+    $file_text .= '\n';
+    $characters_left_on_line = 60;
+    while (strlen($seq) > $characters_left_on_line) {
+      $file_text .= substr($seq_to_display, 0, $count);
+      $seq = substr($seq, $count);
+      $characters_left_on_line = 60;
+    }
+    $file_text .= $seq;
   }
 
 
