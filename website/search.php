23f6f67e1e1bc9d835ab54c3776f89cfba5775c8
Soundous <soudous.bb@gmail.com>
2021-12-07 23:28:24 +0100

    -

--- website/search.php
+++ website/search.php
@@ -40,7 +40,7 @@
         <a href="about.php">About</a>
         <a class="disc" href="disconnect.php">Disconnect</a>
     </div>
-    <div align = "right";><tr style="height:10px; text-align:right;">Hello <?php echo $_SESSION['first_name']?></tr><div>
+    <div class = "undertopnav">Hello <?php echo $_SESSION['first_name']?><div>
 
     <!-- Display info box for successful login -->
     <?php
