8a9464ff449aa754fa30dbe0a8193ccbc72a5b00
SimiliSerpent <benjamin.vacus@polytechnique.edu>
2021-12-08 15:24:31 +0100

    Update genome_info.php

--- website/genome_info.php
+++ website/genome_info.php
@@ -65,6 +65,7 @@
           <col span="1" style="width: 7%;">
           <col span="1" style="width: 16%;">
         </colgroup>
+
         <!-- display header line -->
         <thead>
           <tr>
@@ -84,17 +85,20 @@
 
         <tbody>
           <tr>
+
           <?php
-            # retrieve genome sequence
+
+            // import db functions
             include_once 'libphp/dbutils.php';
             connect_db();
+
+            // retrieve genome sequence from the DB
             $query = "SELECT genome_seq FROM database_projet.genome WHERE genome_id = '" . $genome_id . "';";
             $result = pg_query($db_conn, $query) or die('Query failed with exception: ' . pg_last_error());
             $genome_whole_seq = pg_fetch_result($result, 0, 0);
             $genome_size = strlen($genome_whole_seq);
-            #echo substr($genome_whole_seq, 0, 300);
 
-            # display first column
+            // display the position in the genome of the first character of the line in the left-most cell
             echo "<td align='right'>";
             $nb_of_lines = intdiv($genome_size, $char_per_line) + 1;
             for ($line = 0; $line < $nb_of_lines; $line++) {
@@ -102,27 +106,36 @@
               echo "$char-<br>";
             }
             echo '</td>';
+
+            // display genome sequence in the second cell
             echo "<td align='left'>";
 
-            # extend query max time because it takes quite some time to retrieve and display whole genome
+            // extend query max time because it takes quite some time to retrieve and display whole genome
             ini_set('max_execution_time', '300'); //300 seconds = 5 minutes
-            # retrieve all genes
+
+            // retrieve all genes of this genome from the DB
             $query = "SELECT sequence_id, start_seq, end_seq, gene_seq FROM database_projet.gene WHERE genome_id = '" . $genome_id . "' ORDER BY start_seq;";
             $result = pg_query($db_conn, $query) or die('Query failed with exception: ' . pg_last_error());
+
+            // stores the position we are at in the genome
             $nucl_ind_count = 0;
+
+            // stores the number of characters left on the current line
             $count = $char_per_line;
+
+            // retrieve gene informations
             for ($gene_ind = 0; $gene_ind < pg_num_rows($result); $gene_ind++) {
 
-              # store gene informations
+              // store gene informations
               $seq_id = pg_fetch_result($result, $gene_ind, 0);
               $seq_start = pg_fetch_result($result, $gene_ind, 1);
               $seq_end = pg_fetch_result($result, $gene_ind, 2);
               $gene_seq = pg_fetch_result($result, $gene_ind, 3);
 
-              # check that sequence does not overlap with previously displayed sequences, else, skip sequence
+              // control if that sequence does not overlap with previously displayed sequences: if it does, skip sequence
               if ($seq_start > $nucl_ind_count) {
 
-                # display intergenic part immediately before gene
+                // display intergenic part immediately before gene, with a given number of characters per line
                 echo '<span style="font-family:Consolas;">'; # set style
                 $seq_to_display = substr($genome_whole_seq, $nucl_ind_count, $seq_start-$nucl_ind_count-1);
                 while (strlen($seq_to_display) > $count) {
@@ -132,31 +145,38 @@
                   $count = $char_per_line;
                 }
                 echo $seq_to_display;
-                $count = $count - strlen($seq_to_display);
-                echo '</span>';
 
+                // actualize number of characters left on the current line
+                $count = $count - strlen($seq_to_display);
+
+                // add link of the sequence page (make the gene clickable)
+                echo '</span>';
                 echo "<a href=\"./sequence_info.php?sid=" . $seq_id . "\" ";
-                # check if gene is annotated
+
+                // check if gene is annotated
                 $query_annot = "SELECT gene_id, gene_symbol, description, annotator FROM database_projet.annotations WHERE sequence_id = '" . $seq_id . "' AND genome_id = '" . $genome_id . "';";
                 $result_annot = pg_query($db_conn, $query_annot) or die('Query failed with exception: ' . pg_last_error());
-                # if it's not...
+
+                // if it's not, display warning in mouse-over text and set color to red
                 if(pg_num_rows($result_annot) == 0) {
                   $color = "red";
                   $info = ' title="' . "WARNING: Unannotated gene";
                   echo 'style="font-family:Consolas;color:' . $color . ';"' . $info . '">';
                 }
-                # if it is...
+
+                // if it is, display informations in mouse-over text and set color to blue
                 else {
                   $color = "blue";
                   $info = ' title="';
-                  # add gene symbol if it exists
+                  // add gene symbol if it exists
                   if (pg_fetch_result($result_annot, 0, 1) != "") {
                     $info = $info . pg_fetch_result($result_annot, 0, 1) . "\n";
                   }
                   $info = $info . pg_fetch_result($result_annot, 0, 0) . "\n" . pg_fetch_result($result_annot, 0, 2) . "\nClick to see annotation";
                   echo 'style="font-family:Consolas;color:' . $color . ';"' . $info . '">';
                 }
-                # display gene
+
+                // display gene sequence, with a given number of characters per line
                 $seq_to_display = $gene_seq;
                 while (strlen($seq_to_display) > $count) {
                   echo substr($seq_to_display, 0, $count);
@@ -165,12 +185,17 @@
                   $count = $char_per_line;
                 }
                 echo $seq_to_display;
+
+                // actualize number of characters left on the current line
                 $count = $count - strlen($seq_to_display);
                 echo '</a>';
+
+                // actualize the position we are at in the genome
                 $nucl_ind_count = $seq_end;
               }
             }
-            # display end of genome
+
+            // display end of genome, with a given number of characters per line
             echo '<span style="font-family:Consolas;">';
             $seq_to_display = substr($genome_whole_seq, $nucl_ind_count);
             while (strlen($seq_to_display) > $count) {
@@ -180,11 +205,13 @@
               $count = $char_per_line;
             }
             echo $seq_to_display;
+
+            // actualize number of characters left on the current line
             $count = $count - strlen($seq_to_display);
             echo '</span>';
             echo '</td>';
 
-            # display third column
+            // display the position in the genome of the last character of the line in the third cell
             echo "<td align='left'>";
             $nb_of_lines = intdiv($genome_size, $char_per_line) + 1;
             for ($line = 1; $line < $nb_of_lines; $line++) {
@@ -194,6 +221,8 @@
             echo "-$genome_size";
             echo '</td>';
           ?>
+
+          <!-- display the sequence id (and gene symbol when available) for each sequence in the right-most cell -->
           <td>
             <?php
               $line_ind = 0;
