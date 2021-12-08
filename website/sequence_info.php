62d7a961a45236ac1f8f4b1fca1cfbed5866383b
SimiliSerpent <benjamin.vacus@polytechnique.edu>
2021-12-08 12:45:59 +0100

    Update sequence_info.php

--- website/sequence_info.php
+++ website/sequence_info.php
@@ -118,7 +118,7 @@
                 // display warning if annotation is not validated yet
                 if (pg_fetch_result($result_annot, 0, 8) == 'waiting') {
                   $waiting = true;
-                  echo '<span style="color:orange;">' . "<br>WARNING: THIS ANNOTATION HAS NOT BEEN VALIDATED YET! Use with caution.<br><br>";
+                  echo '<span style="color:#781D42;">' . "<br>WARNING: THIS ANNOTATION HAS NOT BEEN VALIDATED YET! Use with caution.<br><br>";
                 }
 
                 // display gene biotype
