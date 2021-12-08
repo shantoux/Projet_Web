26401727bb510a1c8b665565e71fce539f50b37a
shantoux <sandra_95170@hotmail.com>
2021-12-08 18:49:55 +0100

    debug

--- website/sequence_annotation.php
+++ website/sequence_annotation.php
@@ -75,7 +75,7 @@ if (!isset($_SESSION['user'])) {
     /////Retrieve latest attempt number
     $query_attempt = "SELECT a.attempt
       FROM database_projet.annotations a
-      WHERE genome_id = '" . $_GET['gid'] . "' AND sequence_id = '" . $_GET['sid'] . "' AND status == 'assigned';";
+      WHERE genome_id = '" . $_GET['gid'] . "' AND sequence_id = '" . $_GET['sid'] . "' AND status = 'assigned';";
     $result_attempt = pg_query($db_conn, $query_attempt) or die('Query failed with exception: ' . pg_last_error());
     $attempt = pg_fetch_result($result_attempt, 0, 0);
 
