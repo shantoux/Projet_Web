81ec1dd119853b7bfd6d7ecb58043b00b2d6f13c
SimiliSerpent <benjamin.vacus@polytechnique.edu>
2021-12-08 11:11:44 +0100

    Update search_result.php

--- website/search_result.php
+++ website/search_result.php
@@ -192,7 +192,7 @@
           }
           ### query finally is complete! Add last ';'
           $query = $query . ";";
-          echo $query;
+          echo '<td>' . $query . '</td>';
 
           // send the query to the DB
           $result = pg_query($db_conn, $query) or die('Query failed with exception: ' . pg_last_error());
