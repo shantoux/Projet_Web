b863b7d9a9b31ff8ecf326269b137e9cca38f143
SimiliSerpent <benjamin.vacus@polytechnique.edu>
2021-12-08 23:44:55 +0100

    Update sequence_info.php

--- website/sequence_info.php
+++ website/sequence_info.php
@@ -215,8 +215,7 @@
     $html = file_get_html($adress);
     $uniprot_protein_name = $html->find(".entryID", 0)->plaintext;
 
-    $adress = 'https://www.uniprot.org/uniprot/' . $uniprot_protein_name;
-
+    $adress = 'http://pfam.xfam.org/protein/' . $uniprot_protein_name;
 
     echo '<div class="center">';
     echo $adress . '<br>';
