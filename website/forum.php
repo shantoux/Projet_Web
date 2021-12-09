a5240d8f5941689efa24827c42e1e3ea3ee16921
SimiliSerpent <benjamin.vacus@polytechnique.edu>
2021-12-10 00:25:42 +0100

    Update forum.php

--- website/forum.php
+++ website/forum.php
@@ -57,7 +57,7 @@
       </div>
 
       <div class="center">
-        This is the annotation forum. Create a conversation with other annotators to help on any question or difficulty.<br>
+        This is the annotation forum. Create a conversation with other annotators to help on any question or difficulty.<br><br><br>
       </div>
 
       <?php
@@ -119,7 +119,7 @@
         }
         // chose conversation participants and topic name if the topic instanciation button has been clicked
         else {
-          echo 'Chose who will be part of the conversation:<br><br><br>';
+          echo 'Chose who will be part of the conversation:<br>';
           echo '<span class="small_text">Hold \'ctrl\' to select multiple users</span><br>';
           echo '<form action="forum.php" method = "post">';
           echo '<select name="selected_users[]" ';
