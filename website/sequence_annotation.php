e4f2840f67f402e930a8eab69a6f9bdac3c00432
shantoux <sandra_95170@hotmail.com>
2021-12-09 09:10:07 +0100

    version moche

--- website/sequence_annotation.php
+++ website/sequence_annotation.php
@@ -140,9 +140,16 @@ if (isset($_POST['send_annotation']) || isset($POST['save_annotation'])) {
     <?php echo 'Sequence is ' . strlen($nt) . ' nucleotides long - it starts on position <b>' . $start . '</b> and ends on position <b>' . $end . '</b>.<br><br>';?>
 
     <?php if ($status == 'assigned'){
-      echo '<form action="./sequence_annotation.php?gid=' . $genome_id . '&sid=' . $sequence_id . '" method="post">';
-      echo '<b>Gene identifier : </b><input type="text" name="gene_id" 
-                    value = "'.(isset($_POST['gene_id']))? htmlspecialchars($_POST['gene_id']) : $gene_id .'"><br>';  
+      echo '<form action="./sequence_annotation.php?gid=' . $genome_id . '&sid=' . $sequence_id . '" method="post">';?>
+      <b>Gene identifier : </b><input type="text" name="gene_id" 
+                    value = "<?php echo(isset($_POST['gene_id']))? htmlspecialchars($_POST['gene_id']) : $gene_id ?>"> <br>
+      <b>Gene biotype : </b><input type="text" name="gene_id" 
+                    value = "<?php echo(isset($_POST['gene_id']))? htmlspecialchars($_POST['gene_id']) : $gene_biotype ?>"> <br>
+      <b>Gene symbol : </b><input type="text" name="gene_id" 
+                    value = "<?php echo(isset($_POST['gene_id']))? htmlspecialchars($_POST['gene_id']) : $gene_symbol ?>"> <br>
+      <b>Description : </b><input type="text" name="gene_id" 
+                    value = "<?php echo(isset($_POST['gene_id']))? htmlspecialchars($_POST['gene_id']) : $description ?>"> <br>             
+      <?php
       echo '<b>Gene biotype : </b><input type="text" name="gene_biotype"><br>';
       echo '<b>Transcript biotype : </b><input type="text" name="transcript_biotype"><br>';
       echo '<b> Gene symbol : </b><input type ="text" name = "gene_symbol"><br>';
