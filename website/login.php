fe128cd4c79688ab9f9d9b193348e2220146d1c9
Soundous <soudous.bb@gmail.com>
2021-12-09 12:25:19 +0100

    fix

--- website/login.php
+++ website/login.php
@@ -25,20 +25,19 @@
   <?php
   include_once 'libphp/dbutils.php';
   connect_db(); // connexion to database
-
   if (isset($_POST['submit'])){
     //Get email and password filled in the connexion form
     $user_name = $_POST["name"];
     $user_password = $_POST["pass"];
 
     // Query : Select all user info for a specified email and password
-    $query = "SELECT * FROM database_projet.users WHERE email = '$user_name';"; // AND pw = '$user_password';";
+    $query = "SELECT * FROM database_projet.users WHERE email = '$user_name' AND pw = '$user_password';";
     $result = pg_query($db_conn, $query) or die('Query failed with exception: ' . pg_last_error());
 
     $hash = pg_fetch_result($result, 0, 1);
+  }
 
-
-    if(password_verify($user_password, $hash)){
+  if (isset($_POST['submit']) && password_verify($user_password, $hash)) {
       if (pg_num_rows($result) == 1) {
         //If there's only one result to the query = correct pair of email/pw
         $validated = pg_fetch_result($result, 0, 6) == 'validated'; //get the result of the 7th column (Status) for the 1st row
@@ -59,62 +58,60 @@
 
           # display message
           echo "<div class=\"alert_bad\">
+            <span class=\"closebtn\"
+            onclick=\"this.parentElement.style.display='none';\">&times;</span>
+            Your account has not been validated by an admin yet.
+            </div>";
+          }
+      } else {
+        // If there's no result to the query : wrong pair of email/pw
+        echo "<div class=\"alert_bad\">
           <span class=\"closebtn\"
           onclick=\"this.parentElement.style.display='none';\">&times;</span>
-          Your account has not been validated by an admin yet.</div>";
+          Wrong Username or Password.</div>";
+      }
+    } else if (isset($_POST['submit'])) {
+      //Get email and password filled in the connexion form
+      //$user_name = $_POST["name"];
+      //$user_password = $_POST["pass"];
+      // Query : Select all user info for a specified email and password
+      $query = "SELECT * FROM database_projet.users WHERE email = '$user_name' AND pw = '$user_password';";
+      $result = pg_query($db_conn, $query) or die('Query failed with exception: ' . pg_last_error());
 
-          } else {
-            // If there's no result to the query : wrong pair of email/pw
-            echo "<div class=\"alert_bad\"><span class=\"closebtn\"
-            onclick=\"this.parentElement.style.display='none';\">&times;</span>
-            Wrong Username or Password.</div>";
-          }
+      if (pg_num_rows($result) == 1) {
+        //If there's only one result to the query = correct pair of email/pw
+        $validated = pg_fetch_result($result, 0, 6) == 'validated'; //get the result of the 7th column (Status) for the 1st row
 
+        if ($validated) {
+          // If the user's status is "validated" (approved by the site's admin)
+          // Go to the search page
+          echo '<script>location.href="search.php"</script>';
 
+          // Start a session and store variables email and role
+          session_start();
+          $_SESSION['user'] = $_POST['name'];
+          $_SESSION['role'] = pg_fetch_result($result, 0, 5);
+          $_SESSION['first_name'] = pg_fetch_result($result, 0, 3);
+          $_SESSION['last_name'] = pg_fetch_result($result, 0, 2);
         } else {
-          //Get email and password filled in the connexion form
-          //$user_name = $_POST["name"];
-          //$user_password = $_POST["pass"];
-          // Query : Select all user info for a specified email and password
-          $query = "SELECT * FROM database_projet.users WHERE email = '$user_name';"; // AND pw = '$user_password';";
-          $result = pg_query($db_conn, $query) or die('Query failed with exception: ' . pg_last_error());
+          // If the user has not been approved yet
 
-          if (pg_num_rows($result) == 1) {
-            //If there's only one result to the query = correct pair of email/pw
-            $validated = pg_fetch_result($result, 0, 6) == 'validated'; //get the result of the 7th column (Status) for the 1st row
-
-            if ($validated) {
-              // If the user's status is "validated" (approved by the site's admin)
-              // Go to the search page
-              echo '<script>location.href="search.php"</script>';
-
-              // Start a session and store variables email and role
-              session_start();
-              $_SESSION['user'] = $_POST['name'];
-              $_SESSION['role'] = pg_fetch_result($result, 0, 5);
-              $_SESSION['first_name'] = pg_fetch_result($result, 0, 3);
-              $_SESSION['last_name'] = pg_fetch_result($result, 0, 2);
-
-            } else {
-              // If the user has not been approved yet
-
-              # display message
-              echo "<div class=\"alert_bad\">
-                <span class=\"closebtn\"
-                onclick=\"this.parentElement.style.display='none';\">&times;</span>
-                Your account has not been validated by an admin yet.
-                </div>";
-              }
-            } else {
-              // If there's no result to the query : wrong pair of email/pw
-              echo "<div class=\"alert_bad\">
-              <span class=\"closebtn\"
-              onclick=\"this.parentElement.style.display='none';\">&times;</span>
-              Wrong Username or Password.</div>";
-            }
+          # display message
+          echo "<div class=\"alert_bad\">
+            <span class=\"closebtn\"
+            onclick=\"this.parentElement.style.display='none';\">&times;</span>
+            Your account has not been validated by an admin yet.
+            </div>";
           }
+        } else {
+          // If there's no result to the query : wrong pair of email/pw
+          echo "<div class=\"alert_bad\">
+          <span class=\"closebtn\"
+          onclick=\"this.parentElement.style.display='none';\">&times;</span>
+          Wrong Username or Password.</div>";
         }
-        ?>
+      }
+      ?>
 
   <!-- Login form -->
   <div id="element1">
