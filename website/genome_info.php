7642868c03c4801f22f848e5dc8632200cbf2191
Soundous <soudous.bb@gmail.com>
2021-12-10 19:31:25 +0100

    added charset

--- website/genome_info.php
+++ website/genome_info.php
@@ -14,7 +14,7 @@
 
   <!-- Page header -->
   <head>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="UTF-8" name="viewport" content="width=device-width, initial-scale=1">
     <title>Genome information </title>
     <link rel="stylesheet" type="text/css" href="./style.css" />
   </head>
