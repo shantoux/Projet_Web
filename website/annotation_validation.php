d7dd946644460c58fd2007f5404f8a0e3afbbac8
Soundous <soudous.bb@gmail.com>
2021-12-10 14:10:23 +0100

    --

--- website/annotation_validation.php
+++ website/annotation_validation.php
@@ -199,7 +199,7 @@ connect_db(); ?>
               echo "<td>";
               echo "<div style=\"float:left; width: 50%;\">";
               echo '<input class="button_ok" ype="submit" name="Accept_button" value="Accept"></div>';
-              echo "<div style=\"float: left; width: auto;\">";
+              //echo "<div style=\"float: left; width: auto;\">";
               echo '<input class="button_red" type="submit" name="Reject_button" value="Reject"> </form> </div>';
               echo "</td>";
               echo "</tr>";
