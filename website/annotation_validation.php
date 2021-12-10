d8557c3486949e0108f8bef83609f15877608ed2
Soundous <soudous.bb@gmail.com>
2021-12-10 14:15:36 +0100

    --

--- website/annotation_validation.php
+++ website/annotation_validation.php
@@ -197,7 +197,7 @@ connect_db(); ?>
               echo '<td> <form action="annotation_validation.php?gid=' . $rows['genome_id'] .'&sid=' . $rows["sequence_id"] . '&att=' .$rows['attempt']. '&annotator=' . $rows["annotator"] . '" method = "post">';
               echo "<textarea id=\"" . $rows["sequence_id"] . "\" name=\"comments\" cols=\"40\" rows=\"3\" required>" . $rows['comments'] . "</textarea></td>";            # Validate / Refuse annotation
               echo "<td>";
-              echo "<div style=\"float:left; width: 30%;\">";
+              echo "<div style=\"float:left;\">";
               echo '<input class="button_ok" ype="submit" name="Accept_button" value="Accept"></div>';
               echo "<div style=\"float: left; width: auto;\">";
               echo '<input class="button_red" type="submit" name="Reject_button" value="Reject"> </form> </div>';
