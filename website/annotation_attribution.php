4434953865a2f4e9b0dbc1354f2c4897c392490f
Soundous <soudous.bb@gmail.com>
2021-12-10 09:42:43 +0100

    removed close button

--- website/annotation_attribution.php
+++ website/annotation_attribution.php
@@ -191,7 +191,6 @@
 
           # display message
           echo "<div class=\"alert_neutral\">
-          <span class=\"closebtn\" onclick=\"this.parentElement.style.display='none';\">&times;</span>
           There is no new sequences to attribute.</div>";
         } //<span class=\"closebtn\" onclick=\"this.parentElement.style.display='none';\">&times;</span>
         echo '</tbody>';
